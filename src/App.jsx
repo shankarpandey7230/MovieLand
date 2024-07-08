01307870e3308348243ca492e040dfef3ac836ff
shankarpandey7230 <shankarpandey7230@gmail.com>
2024-07-08 12:12:45 +1000

    setting up the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>Hello there</div>
+    </>
+  );
+}
+
+export default App;
